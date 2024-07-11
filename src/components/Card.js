b4d56a70aac49137b3d1a9394694d60d30726ba4
Tarique Anwer <itzmetariq@gmail.com>
2024-07-11 07:05:03 +0530

    added Card.js

---
+++ src/components/Card.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { useDrag, useDrop } from "react-dnd";
+import "./Card.css";
+
+const Card = ({ item, index, moveCard, onClick }) => {
+  const ref = React.useRef(null);
+
+  const [, drop] = useDrop({
+    accept: "card",
+    hover(draggedItem) {
+      if (draggedItem.index !== index) {
+        moveCard(draggedItem.index, index);
+        draggedItem.index = index;
+      }
+    },
+  });
+
+  const [{ isDragging }, drag] = useDrag({
+    type: "card",
+    item: { type: "card", index },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+
+  drag(drop(ref));
+
+  const opacity = isDragging ? 0.4 : 1;
+  const thumbnail = `thumbnail${index + 1}.jpg`;
+
+  return (
+    <div
+      ref={ref}
+      className="card"
+      style={{ opacity }}
+      onClick={() => onClick(thumbnail)}
+    >
+      <img src={thumbnail} alt={item.title} className="thumbnail" />
+      <div>{item.title}</div>
+    </div>
+  );
+};
+
+export default Card;
